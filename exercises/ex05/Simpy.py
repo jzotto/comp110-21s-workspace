3cf74c9340a9f868269b6b6c63f5d20db2555fc4
Jenna Zottoli <jzotto@live.unc.edu>
2021-04-19 17:52:25 -0400

    update 4/19

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -4,10 +4,123 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730163077"
 
 
 class Simpy:
     values: list[float]
+    x: float
+    limit: int
+    start: float
+    stop: float
+    step: float
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float]):
+        """Initizalizes constructor."""
+        self.values = values
+
+    def __repr__(self) -> str:
+        """Method to represent object as string."""
+        return f"Simpy({self.values})"
+
+    def fill(self, x: float, limit: int) -> None:
+        """Fills values with a specific number of repeating values."""
+        self.values = []
+        for i in range(limit):
+            self.values.append(x)
+
+    def arange(self, start: float, stop: float, step: float = 1.0):
+        """Fills in values with range of values."""
+        assert step != 0.0
+        self.values = []
+        r = start
+        x = step
+        if r < stop:
+            while r < stop:
+                self.values.append(r)
+                r += step
+        else:
+            while r > stop: 
+                self.values.append(r)
+                r += step
+    
+
+    def sum(self) -> float:
+        """Compute and returns the sum of all items in the values attribute."""
+        return sum(self.values)
+
+
+    def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """ Adds up items in lists."""
+        result: list[float] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item + rhs)
+        else:
+            assert len(self.values) == len(rhs.values) # makes sure they are the same length or else program will fail
+            for i in range(len(self.values)): # starts at 0 but stops at n-1, in s it would stop at 2
+                result.append(self.values[i] + rhs.values[i])
+        return Simpy(result)
+
+        
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy: 
+        """Takes two lists or a float and puts one to the exponent of the other."""
+        result: list[float] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item ** rhs)
+        else:
+            assert len(self.values) == len(rhs.values) # makes sure they are the same length or else program will fail
+            for i in range(len(self.values)): # starts at 0 but stops at n-1, in s it would stop at 2
+                result.append(self.values[i] ** rhs.values[i])
+        return Simpy(result)
+
+
+    def __mod__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Takes the remainder."""
+        result: list[float] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item % rhs)
+        else:
+            assert len(self.values) == len(rhs.values) # makes sure they are the same length or else program will fail
+            for i in range(len(self.values)): # starts at 0 but stops at n-1, in s it would stop at 2
+                result.append(self.values[i] % rhs.values[i])
+        return Simpy(result)
+
+
+    def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        result: list[bool] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item == rhs)
+        else:
+            assert len(self.values) == len(rhs.values) # makes sure they are the same length or else program will fail
+            for i in range(len(self.values)): # starts at 0 but stops at n-1, in s it would stop at 2
+                result.append(self.values[i] == rhs.values[i])
+        return result
+
+    
+    def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        result: list[bool] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                result.append(item > rhs)
+        else:
+            assert len(self.values) == len(rhs.values) 
+            for i in range(len(self.values)):
+                result.append(self.values[i] > rhs.values[i])
+        return result
+
+    
+    def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Overload the subscription notation."""
+        if isinstance(rhs, int):
+            return self.values(rhs)
+
+        elif is instance(rhs, list[bool]): 
+            return self.values[rhs]
+            
+            
+
+
